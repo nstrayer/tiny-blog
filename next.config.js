fe86c2b62cda967a920dd4676c5e0889afb385e2
Nick Strayer <nick@Nicks-Mac-mini.local>
2023-06-19 22:18:06 -0400

    Added build in a dist folder for ghpages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export"
+    output: "export",
+    distDir: 'dist',
+    basePath: '/tiny-blog',
 }
 
 module.exports = nextConfig
